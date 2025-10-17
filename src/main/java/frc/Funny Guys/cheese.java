b3f42920f12d5a5b78f46c6bceb386838a9cb02a
student <hcampbellrapp@icloud.com>
2025-10-16 19:04:43 -1000

    no more funny:(

--- src/main/java/frc/Funny Guys/cheese.java
+++
@@ -1,7 +0,0 @@
-package frc.robot;
-
-public class cheese {
-    public cheese(){
-        System.out.println ("fffffffff");
-    }
-}
--- src/main/java/frc/Funny Guys/iLikeCheese.java
+++
@@ -1,5 +0,0 @@
-package frc.robot;
-
-public class iLikeCheese {
-    int apples = 20000;
-}

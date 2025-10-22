55879e940d0c397c08e3245ac812df2c675072e4
student <ltakamori2@gmail.com>
2025-10-21 19:09:09 -1000

    Aidric >:D

---
+++ src/main/java/frc/robot/Subsystems/PivotSubsystem.java
@@ -0,0 +1,10 @@
+package frc.robot.Subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2,command.SubsystemBase;
+
+public class PivotSubsystem {
+    
+}

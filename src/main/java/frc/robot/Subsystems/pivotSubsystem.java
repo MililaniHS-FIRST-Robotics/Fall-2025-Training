dc1c264f1d1b82e449393b062104210c084e6edc
student <hcampbellrapp@icloud.com>
2025-11-18 19:20:39 -1000

    cheese25

--- src/main/java/frc/robot/Subsystems/pivotSubsystem.java
+++ src/main/java/frc/robot/Subsystems/pivotSubsystem.java
@@ -1,17 +1,29 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class pivotSubsystem extends SubsystemBase{
+    private double targetPosition, error, kp, ki, kd, kff, output;
     private SparkMax motor1;
     private SparkMax motor2;
+    private AbsoluteEncoder encoder1;
     public pivotSubsystem(){
         motor1 = new SparkMax(2, MotorType.kBrushless);
         motor2 = new SparkMax(3, MotorType.kBrushless);
+
+        encoder1 = motor1.getAbsoluteEncoder();
+
+        targetPosition = 40;
+        kp = .1;
+        ki = 0;
+        kd = 0;
+        kff = 0;
     }
 
     public void setPowe(double power){
@@ -23,5 +35,13 @@ public class pivotSubsystem extends SubsystemBase{
     }
     public void setMotor2(double power){
         motor2.set(power);
+    }
+    public double getMotorPosition(){
+        return encoder1.getPosition();
+    }
+
+    public void periodic(){
+        error = targetPosition - getMotorPosition();
+        output = error * kp + kff;
     }
 }

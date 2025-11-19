64ccdf87565eb3a3140b941a55b93e0239031265
student <isaiah.gam3r808@gmail.com>
2025-11-18 19:59:47 -1000

    PID training/mockbuild of 11/18

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,6 +27,7 @@ public class RobotContainer {
 
 
 
+    
 
 
 
--- src/main/java/frc/robot/Subsystems/PivotSubsystem.java
+++ src/main/java/frc/robot/Subsystems/PivotSubsystem.java
@@ -1,25 +1,43 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PivotSubsystem extends SubsystemBase{
     private SparkMax motor1;
     private SparkMax motor2;
+
+    private AbsoluteEncoder encoder1;
+
+    private double targetPosition, error, kp, ki, kd, kff, output;
     public PivotSubsystem(){
        motor1 = new SparkMax(2, MotorType.kBrushless); //ID are placeholders
        motor2 = new SparkMax(3, MotorType.kBrushless);
-    }
-    public void setPower(double power){
-        motor1.set(power);
-        motor2.set(power);
+
+       encoder1 = motor1.getAbsoluteEncoder();
+        targetPosition = 60;
+        kp = .001;
+        ki = 0;
+        kd = 0;
+        kff = 0;
+        output = 0;
+
+
     }
     public void setMotor1(double power){
         motor1.set(power);
     }
     public void setMotor2(double power){
         motor2.set(power);
+    }
+    public double getMotorPosition(){
+        return encoder1.getPosition();
+    }
+    public void periodic(){
+        error = targetPosition - getMotorPosition();
+        output = error * kp + kff;
     }
 }
